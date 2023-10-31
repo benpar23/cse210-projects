70b21c729019d916bb3ea485483bfe8c5ab5fee2
benpar23 <benparada23@gmail.com>
2023-10-30 21:52:55 -0400

    Started building out a letter grade project

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string grade = Console.ReadLine();
+
+        int gradePercent = int.Parse(grade);
+
+        if (gradePercent < 60)
+        {
+            string letterGrade = "F";
+        }
+        else if (gradePercent < 70)
+        {
+            string letterGrade = "D";
+        }
+        else if (gradePercent < 80)
+        {
+            string letterGrade = "C";
+        }
+
     }
 }
