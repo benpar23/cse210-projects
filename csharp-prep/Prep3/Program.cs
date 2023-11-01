cd66bf49f732eb643e3c194ec949ca9f04dac52d
benpar23 <benparada23@gmail.com>
2023-10-31 21:20:14 -0400

    Created the number generator project

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+
+        int magicNumber = randomGenerator.Next(1,100);
+
+        Console.Write("What is your guess? ");
+        string guess = Console.ReadLine();
+        int guessNumber = int.Parse(guess);
+
+
+        while (guessNumber != magicNumber)
+        {    if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+                Console.Write("What is your guess? ");
+                guess = Console.ReadLine();
+                guessNumber = int.Parse(guess);
+            }
+            else if (guessNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+                Console.Write("What is your guess? ");
+                guess = Console.ReadLine();
+                guessNumber = int.Parse(guess);
+            }
+        }
+
+        Console.WriteLine("You guessed it!");
     }
 }
