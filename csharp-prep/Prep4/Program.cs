c9f0add98debab7e712812daccc7ee1d37cec957
benpar23 <benparada23@gmail.com>
2023-11-01 21:24:52 -0400

    Created the number calculator program

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,60 @@
 using System;
 
+using System.Collections.Generic;
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+        List<int> numbers = new List<int>();
+        
+        int number= -1;
+
+        int max = 0;
+
+        int smallest = 999999;
+        
+        Console.WriteLine("Enter a list of numbers. Type 0 when finished.");
+
+        while (number != 0)
+        {
+            number = int.Parse(Console.ReadLine());
+            
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+
+        int sum = 0;
+
+        foreach (int numero in numbers)
+        {
+            sum += numero;
+        }
+
+        float average = ((float)sum)/numbers.Count;
+
+        foreach (int smallNumber in numbers)
+        {
+            if (smallNumber < smallest && smallNumber > 0)
+            {
+                smallest = smallNumber;
+            }
+        }
+
+        foreach (int bigNumber in numbers)
+        {
+            if (bigNumber > max)
+            {
+                max = bigNumber;
+            }
+        }
+
+        Console.WriteLine($"The sum is: {sum}.");
+        Console.WriteLine($"The average is: {average}.");
+        Console.WriteLine($"The largest number is {max}.");
+        Console.WriteLine($"The smallest number is {smallest}.");
     }
 }
