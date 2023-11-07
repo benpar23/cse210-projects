38e0e8980c5fcbc765e3780e70a00c630181c058
benpar23 <benparada23@gmail.com>
2023-11-06 21:50:27 -0500

    Created the jobs program

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+
+        job1._jobTitle = "Software Developer";
+        job1._company = "Microsoft";
+        job1._startYear = 2003;
+        job1._endYear = 2006;
+
+        Job job2 = new Job();
+
+        job2._jobTitle = "Junior Programmer";
+        job2._company = "Google";
+        job2._startYear = 2000;
+        job2._endYear = 2003;
+
+        job1.DisplayJobDetails();
+        job2.DisplayJobDetails();
     }
 }
