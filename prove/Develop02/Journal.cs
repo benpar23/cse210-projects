6fc84f54ff058626f73eecc172dd0a18ecaddfea
benpar23 <benparada23@gmail.com>
2023-11-08 21:23:38 -0500

    added stubs to files

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,6 +1,8 @@
 public class Journal
 {
-    public void AddEntry()
+    public List<Entry> _entries;
+
+    public void AddEntry(Entry newEntry)
     {
 
     }
@@ -8,12 +10,12 @@ public class Journal
     {
 
     }
-    public void SaveToFile()
+    public void SaveToFile(string file)
     {
 
     }
-    public void LoadFromFile()
+    public void LoadFromFile(string file)
     {
-        
+
     }
 }
