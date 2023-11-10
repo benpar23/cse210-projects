3c118ba6091822d724ababbee7133e4f27b49e7c
Ben <benparada23@gmail.com>
2023-11-09 20:03:44 -0500

    added more code to journal project

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,10 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Entry newEntry = new Entry();
-
-        newEntry._date = DateTime.Now.ToString("dd MMMM yyyy");
-
-        Console.WriteLine($"{newEntry._date}");
+        
     }
 }
