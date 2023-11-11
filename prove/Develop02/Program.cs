a823d2fb92a3d86aaa2c843f3f1be2da70cb8994
benpar23 <benparada23@gmail.com>
2023-11-11 11:01:19 -0500

    finished methods in PromptGenerator

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        PromptGenerator _prompt = new PromptGenerator();
+
+        Console.WriteLine(_prompt.GetRandomPrompt());
     }
 }
--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -1,9 +1,14 @@
+using System.Collections.Generic;
 public class PromptGenerator
 {
-    public List<string> _prompts;
+    public List<string> _prompts = new List<string> {"Who was the most interesting person I interacted with today?", "What was the best part of my day?", "What was the strongest emotion I felt today?", "How did I see the hand of the Lord in my life today?", "If I had one thing I could do over today, what would it be?"};
 
     public string GetRandomPrompt()
     {
-        return "";
+        var random = new Random();
+
+        int index = random.Next(_prompts.Count);
+
+        return _prompts[index];
     }
 }
