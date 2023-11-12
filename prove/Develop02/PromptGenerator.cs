571032ac6ff4fd4fe531a3bdc39226d51b97cbbb
benpar23 <benparada23@gmail.com>
2023-11-11 22:19:37 -0500

    Added some more prompts to the generator

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 public class PromptGenerator
 {
-    public List<string> _prompts = new List<string> {"Who was the most interesting person I interacted with today?", "What was the best part of my day?", "What was the strongest emotion I felt today?", "How did I see the hand of the Lord in my life today?", "If I had one thing I could do over today, what would it be?"};
+    public List<string> _prompts = new List<string> {"Who was the most interesting person I interacted with today?", "What was the best part of my day?", "Why did you feel like you wanted to write in your journal today?", "How did I see the hand of the Lord in my life today?", "What was your favourite meal you had today?", "What was the funniest thing that happened today?", "Who did you feel like you emulated in your actions today?", "Who did you talk with the most today?"};
 
     public string GetRandomPrompt()
     {
