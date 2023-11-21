348cda997a5b3b0468e713b2c71ade6773fd7001
Ben <benparada23@gmail.com>
2023-11-20 21:31:59 -0500

    Finished up inheritance learning activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,8 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Assignment homework = new Assignment("Ben Parada", "Geography");
+        Assignment homework = new Assignment ("Ben Parada", "English");
+
+        MathAssignment math = new MathAssignment ("Autumn Parada", "Multiplication", "8.2", "20-25");        
+        
+        WritingAssignment essay = new WritingAssignment("Estrella Parada", "Geography", "The World is Not Flat");
 
         Console.WriteLine(homework.GetSummary());
+
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        Console.WriteLine(essay.GetSummary());
+        Console.WriteLine(essay.GetWritingInformation());
     }
 }
