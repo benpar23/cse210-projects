78a6cee32e768b32656b48f48cdb53f83dedb744
benpar23 <benparada23@gmail.com>
2023-11-21 20:25:54 -0500

    Created all the classes for the Mindfulness program

---
+++ prove/Develop04/ListingActivity.cs
@@ -0,0 +1,27 @@
+public class ListingActivity : Activity
+{
+    int _count;
+    List<string> _prompts;
+
+    public ListingActivity()
+    {
+
+    }
+
+    public void Run()
+    {
+
+    }
+
+    public void GetRandomPrompt()
+    {
+
+    }
+
+    public List<string> GetListFromUser()
+    {
+        List<string> list = new List<string>();
+
+        return list;
+    }
+}
