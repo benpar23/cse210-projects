161bb19d144f13bc59936a11012ae0ffacde9d0c
benpar23 <benparada23@gmail.com>
2023-12-02 17:55:32 -0500

    Added comments in program class

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -3,7 +3,9 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
+    {   
+        // To exceed program requirements, I added the LevelUp method in the GoalManager class and the ability to showcase what level the user is at based on how many points they've accumulated.
+        
         GoalManager goals = new GoalManager();
 
         goals.Start();
