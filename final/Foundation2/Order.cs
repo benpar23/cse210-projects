bcc4b60a17d0616f2145bd6be8aa527b7e24c5e4
benpar23 <benparada23@gmail.com>
2023-12-04 22:55:18 -0500

    Started on Foundation2 of final project

---
+++ final/Foundation2/Order.cs
@@ -0,0 +1,62 @@
+public class Order
+{
+    private List<Product> _products = new List<Product>();
+    private Customer _customer;
+
+    public Order(Customer customer)
+    {
+        _customer = customer;
+    }
+
+    public double GetTotal()
+    {
+        double total = 0;
+        
+        foreach (Product product in _products)
+        {
+            total += product.GetSubtotal();
+        }
+
+        if (_customer.GetAddress().IsUSA())
+        {
+            total += 5;
+        }
+        else
+        {
+            total += 35;
+        }
+
+        return total;
+    }
+
+    public string GetPackingLabel()
+    {
+        string label = "";
+
+        int i = 0;
+        
+        foreach (Product product in _products)
+        {
+            i++;
+            string labelInfo = product.GetLabelInfo();
+            label += $"{i}. {labelInfo}";
+        }
+        
+        return label;
+    }
+
+    public string GetShippingLabel()
+    {
+       string customerName = _customer.GetName();
+       Address address = _customer.GetAddress();
+
+       string addressString = address.GetAddress();
+       
+       return $"{customerName}\r\n{addressString}";
+    }
+
+    public void AddProduct(Product product)
+    {
+        _products.Add(product);
+    }
+}
