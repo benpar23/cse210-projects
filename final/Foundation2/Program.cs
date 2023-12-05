7f7072efa206c5e036ef8596204a195c10baf2f8
benpar23 <benparada23@gmail.com>
2023-12-04 23:05:27 -0500

    Basically completed Foundation2 code

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        Address addressOne = new Address("30 Saltzburg Crescent", "Keswick", "ON", "Canada");
+        Customer customerOne = new Customer("Ben Parada", addressOne);
+
+        Product productOne = new Product("Shampoo", "005", 5.49, 2);
+        Product productTwo = new Product("Soap", "004", 2.5, 1);
+        Product productThree = new Product("Hairbrush", "010", 4.99, 1);
+
+        Order orderOne = new Order(customerOne);
+
+        orderOne.AddProduct(productOne);
+        orderOne.AddProduct(productTwo);
+        orderOne.AddProduct(productThree);
+
+        string packingLabel = orderOne.GetPackingLabel();
+        string shippingLabel = orderOne.GetShippingLabel();
+
+        double totalPrice = orderOne.GetTotal();
+
+        Console.WriteLine(packingLabel);
+        Console.WriteLine(totalPrice);
+        Console.WriteLine(shippingLabel);
+
     }
 }
